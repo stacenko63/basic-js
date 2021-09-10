ba13bb0354e854f590caef3f746806248fcf3692
Artem <85314559+stacenko63@users.noreply.github.com>
2021-09-10 14:04:21 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -15,5 +15,23 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default function createDreamTeam(/* members */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  function createDreamTeam(members)
+{
+    let min = ""
+    let answer = ""
+    for (let i = 0; i < members.length - 1; i++)
+    {
+        min = i;
+        for (let j = i+1; j < members.length; j++)
+        {
+            if (members[j] < members[min]) min = j;
+        }
+        [members[i], members[min]] = [members[min], members[i]];
+    }
+    for (let i = 0; i < members.length; i++)
+    {
+        if (typeof(members[i]) == "string") answer += members[i][0];
+    }
+    return answer;
+}
 }
