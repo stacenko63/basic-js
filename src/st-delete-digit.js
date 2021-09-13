e41ac6e0c1a483c4be99f6c0a7e74f72c9e62e37
“stacenko63” <amr_stacenko@mail.ru>
2021-09-13 20:28:31 +0300

    fix: delete-digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(digit) {
+ let arr = [];
+ let string_digit = String(digit); 
+ if (string_digit.length == 2) 
+ {
+     arr.push(Number(string_digit[0]),Number(string_digit[1]));
+ }
+ else for (let i = 1; i < string_digit.length; i++)
+ {
+     arr.push(Number(string_digit.slice(0, i-1) + string_digit.slice(i)));
+ }  
+ let max = arr[0];
+ for (let i = 1; i < arr.length; i++)
+ {
+     if (arr[i] > max) max = arr[i];
+ }
+ return max;
 }
