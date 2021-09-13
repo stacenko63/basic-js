ba5472bbedb05af8fad5edd87f3f06ec316281a2
“stacenko63” <amr_stacenko@mail.ru>
2021-09-13 22:12:15 +0300

    fix: st-mine-sweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,33 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  {
+    let arr = new Array(matrix.length);
+    for (let i = 0; i < arr.length; i++)
+    {
+        arr[i] = new Array(matrix[0].length);
+    }
+    for (let i = 0; i < matrix.length; i++)
+    {
+        for (let j = 0; j < matrix[i].length; j++)
+        {
+            let count = 0;
+            if (matrix[i][j] == true) arr[i][j] = 1;
+            else if (matrix[i][j] == false)
+            {
+                if (i != 0 && matrix[i-1][j] == true) count++;
+                if (i < matrix.length-1 && j < matrix[i].length-1 && matrix[i+1][j+1] == true) count++;
+                if (i < matrix.length-1 && j != 0 && matrix[i+1][j-1] == true) count++;
+                if (i != 0 && j != 0 && matrix[i-1][j-1] == true) count++;
+                if (i != 0 && j < matrix[i].length-1 && matrix[i-1][j+1] == true) count++;
+                if (i < matrix.length-1 && matrix[i+1][j] == true) count++;
+                if (j < matrix[i].length-1 && matrix[i][j+1] == true) count++;
+                if (j != 0 && matrix[i][j-1] == true) count++;
+                arr[i][j] = count;
+            }
+        }
+    }
+    return arr;
+}
 }
