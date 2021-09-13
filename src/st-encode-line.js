7e2cd16b2a87518729a673ee959f48fc725685c1
“stacenko63” <amr_stacenko@mail.ru>
2021-09-13 17:21:26 +0300

    fix: encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let s2 = "";
+    for (let i = 0; i < str.length; i++)
+    {
+        let count = 1;
+        if (str[i] == str[i+1])
+        {
+            while(str[i] == str[i+1])
+            {
+                count++;
+                i++;
+            }
+            s2 += count + str[i];
+            count = 0;
+        }
+        else s2 += str[i];
+    }
+    return s2;
 }
