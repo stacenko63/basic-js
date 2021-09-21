3e39e77e858dac1d15d882f999790f8c73ffd3b3
“stacenko63” <amr_stacenko@mail.ru>
2021-09-21 17:22:32 +0300

    fix: recursive

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,8 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
+export default function dateSample(sampleActivity) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (!isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity >= 15) return false; 
+  return Math.floor(Math.log(15/sampleActivity)/(0.693/5700));
 }
