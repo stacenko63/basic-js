fc53af50388f7c8caf188b67b195143bd6564b11
“stacenko63” <amr_stacenko@mail.ru>
2021-09-22 15:26:43 +0300

    fix: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,72 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  flag;
+  constructor(flag = true)
+  {
+    this.flag = flag;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (typeof(message) == "undefined" || typeof(key) == "undefined") throw new Error ("Incorrect arguments!");
+    let message2 = "";
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 0, j = 0; message2.length < message.length; i++, j++)
+    {
+      if (message.charCodeAt(i) < 65 || message.charCodeAt(i) > 90)
+      {
+        while (message.charCodeAt(i) < 65 || message.charCodeAt(i) > 90) 
+        {
+          message2 += message[i];
+          i++;
+        }
+      }
+      if (j < key.length) message2 += key[j];
+      if (j == key.length - 1) j = -1;
+    }
+    let message3 = "";
+    for (let i = 0; i < message.length; i++)
+    {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90)
+      {
+        if (message.charCodeAt(i)+message2.charCodeAt(i)-65 > 90) message3 += String.fromCharCode((message.charCodeAt(i)+message2.charCodeAt(i)-91));
+        else message3 += String.fromCharCode((message.charCodeAt(i)+message2.charCodeAt(i)-65));
+      }
+      else message3 += message[i];
+    }
+    if (this.flag == false) message3 = message3.split("").reverse().join("");
+    return message3;
+  }
+  decrypt(message, key) {
+    if (typeof(message) == "undefined" || typeof(key) == "undefined") throw new Error ("Incorrect arguments!");
+    let message2 = "";
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 0, j = 0; message2.length < message.length; i++, j++)
+    {
+      if (message.charCodeAt(i) < 65 || message.charCodeAt(i) > 90)
+      {
+        while (message.charCodeAt(i) < 65 || message.charCodeAt(i) > 90) 
+        {
+          message2 += message[i];
+          i++;
+        }
+      }
+      if (j < key.length) message2 += key[j];
+      if (j == key.length - 1) j = -1;
+    }
+    let message3 = "";
+    for (let i = 0; i < message.length; i++)
+    {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90)
+      {
+        if (message.charCodeAt(i)-message2.charCodeAt(i) < 0) message3 += String.fromCharCode((message.charCodeAt(i)-message2.charCodeAt(i)+91));
+        else message3 += String.fromCharCode((message.charCodeAt(i)-message2.charCodeAt(i)+65));
+      }
+      else message3 += message[i];
+    }
+    if (this.flag == false) message3 = message3.split("").reverse().join("");
+    return message3;
   }
 }
+
