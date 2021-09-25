e6bf2f5f4ecc65740f67ea0f6043acb288e90fc5
“stacenko63” <amr_stacenko@mail.ru>
2021-09-25 11:45:12 +0300

    fix: simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    str: "",
+     getLength() {
+       let arr = this.str.split("~~");
+       return arr;
+     },
+     addLink(value) {
+       //('( )')
+     if (this.str.length == 0) this.str += "( " + value + " )";
+      else this.str += "~~( " + value + " )";
+     },
+     removeLink(position) {
+       if (typeof(position) != "number" || Number.isInteger(position)) throw new Error ("You can't remove incorrect link!");
+       let check = false;
+       for (let i = 0 ; i < this.str.length; i++)
+       {
+         if (Number(this.str[i]) == position && isNaN(this.str[i+1]) && isNaN(this.str[i-1])) check = true;
+       }
+       if (check == false) throw new Error ("You can't remove incorrect link!");
+     },
+     reverseChain() {
+       
+   this.str.split("~~").reverse().join("~~");
+     },
+     finishChain() {
+       return this.str;
+     }
 };
